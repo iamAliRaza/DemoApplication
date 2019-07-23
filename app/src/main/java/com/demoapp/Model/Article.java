32b74de1283fb26b2c050d6c81f0d26d630721cb
paranoid <soni.butt007@gmail.com>
2019-07-24 00:25:15 +0400

    Initial Commit

---
+++ app/src/main/java/com/demoapp/Model/Article.java
@@ -0,0 +1,50 @@
+package com.demoapp.Model;
+
+public class Article {
+
+    private String title;
+    private String _abstract;
+    private String pDate;
+    private String byline;
+    private String source;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String get_abstract() {
+        return _abstract;
+    }
+
+    public void set_abstract(String _abstract) {
+        this._abstract = _abstract;
+    }
+
+    public String getpDate() {
+        return pDate;
+    }
+
+    public void setpDate(String pDate) {
+        this.pDate = pDate;
+    }
+
+    public String getByline() {
+        return byline;
+    }
+
+    public void setByline(String byline) {
+        this.byline = byline;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+}
